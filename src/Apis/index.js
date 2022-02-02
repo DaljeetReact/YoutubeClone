7608b1c3e1358086d18de691ac9b2d1393422dbc
singhtrustcom <78361510+singhtrustcom@users.noreply.github.com>
2022-02-02 18:49:57 +0100

    change google api with rapid api

--- src/Apis/index.js
+++ src/Apis/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 export const youtTube = (query='Love Songs')=>{
-   return  axios.get('https://www.googleapis.com/youtube/v3/search',{ 
+   return  axios.get('https://youtube-v31.p.rapidapi.com/search',{ 
        params: {
             part:'snippet',
             maxResults: 5,
-            key:'AIzaSyA0JeivyTh1g8iR1KiCKzzma7rfdjuFBFI',
             q:query,
             safeSearch:'moderate'
+        },
+        headers: {
+            'x-rapidapi-host': 'youtube-v31.p.rapidapi.com',
+            'x-rapidapi-key': 'f074a64f16msh0b559fcfb6b71f4p1dc602jsn91bb5100c3bf'
         }
     });
 }
